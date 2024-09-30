36656117bb72d22cc1f1012ac1ac65a3a64373b4
Kevin Kim <kevinnkimm96@gmail.com>
2024-09-30 23:37:34 +0800

    add apimanager to handle client api calls

---
+++ lib/apiManager.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosError } from "axios";
+
+const baseAPI = axios.create({
+  baseURL: "/api",
+  withCredentials: true,
+});
+
+export const getAPICall = async (url: string) => {
+  try {
+    const res = await baseAPI.get(url);
+    return res.data;
+  } catch (err: unknown) {
+    console.log(err);
+    if (err instanceof AxiosError && err?.response?.status === 403)
+      return { isLoggedIn: false };
+    return { error: err };
+  }
+};
+
+export const postAPICall = async (url: string, dataObj: any) => {
+  try {
+    const formData = new FormData();
+    Object.keys(dataObj).forEach((key) => {
+      const value = dataObj[key];
+      formData.append(key, value);
+    });
+    const res = await baseAPI.post(url, formData);
+    return res.data;
+  } catch (err: unknown) {
+    console.log(err);
+    if (err instanceof AxiosError && err?.response?.status === 403)
+      return { isLoggedIn: false };
+    return { error: err };
+  }
+};
