ca3dddf071fa78c3b37bc7de52b2702fc8a08dd9
Kevin Kim <kevinnkimm96@gmail.com>
2024-09-30 23:37:06 +0800

    add mongodb client

---
+++ lib/mongoDB.ts
@@ -0,0 +1,29 @@
+// mongodb.js
+
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI || "";
+
+let client;
+let clientPromise;
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("Add Mongo URI to .env.local");
+}
+
+let globalWithMongo = global as typeof globalThis & {
+  _mongoClientPromise: Promise<MongoClient>;
+};
+
+if (process.env.NODE_ENV === "development") {
+  if (!globalWithMongo._mongoClientPromise) {
+    client = new MongoClient(uri);
+    globalWithMongo._mongoClientPromise = client.connect();
+  }
+  clientPromise = globalWithMongo._mongoClientPromise;
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
