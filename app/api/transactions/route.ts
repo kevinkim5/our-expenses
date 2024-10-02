65b95aa985d47622f213d650734e9665b0a52590
Kevin Kim <kevinnkimm96@gmail.com>
2024-10-02 22:31:17 +0800

    remove unused vars

--- app/api/transactions/route.ts
+++ app/api/transactions/route.ts
@@ -1,25 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import clientPromise from "../../../lib/mongoDB";
-import { FORM_TYPES } from "@/constants/common";
 
-interface SaveObj {
-  Date?: Date;
-  Amount?: number;
-  Description?: string;
-  Claim?: boolean;
-  Settle?: boolean;
-}
-
-export async function GET(req: NextRequest) {
+export async function GET() {
   const client = await clientPromise;
   const db = client.db("expenses");
 
   const allTransactions = await db
     .collection("transactions")
-    .find({
-      // sort: { Date: 1 },
-      // Claim: true,
-    })
+    .find({})
     .sort({ Date: -1 })
     .toArray();
   return NextResponse.json({ status: 200, data: allTransactions });
