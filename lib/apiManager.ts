ce445cc8b09e70c07e264d59434f921fa2b9cc73
Kevin Kim <kevinnkimm96@gmail.com>
2024-10-21 01:14:16 +0800

    add delete api manager

--- lib/apiManager.ts
+++ lib/apiManager.ts
@@ -1,39 +1,51 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError } from 'axios'
 
 const baseAPI = axios.create({
-  baseURL: "/api",
+  baseURL: '/api',
   withCredentials: true,
-});
+})
 
 type SaveObj = {
-  [key: string]: string;
-};
-  
+  [key: string]: string
+}
+
+export const deleteAPICall = async (url: string) => {
+  try {
+    const res = await baseAPI.delete(url)
+    return res.data
+  } catch (err: unknown) {
+    console.error('deleteAPICall: ', err)
+    if (err instanceof AxiosError && err?.response?.status === 403)
+      return { isLoggedIn: false }
+    throw new Error()
+  }
+}
+
 export const getAPICall = async (url: string) => {
   try {
-    const res = await baseAPI.get(url);
-    return res.data;
+    const res = await baseAPI.get(url)
+    return res.data
   } catch (err: unknown) {
-    console.log(err);
+    console.log(err)
     if (err instanceof AxiosError && err?.response?.status === 403)
-      return { isLoggedIn: false };
-    return { error: err };
+      return { isLoggedIn: false }
+    return { error: err }
   }
-};
+}
 
 export const postAPICall = async (url: string, dataObj: SaveObj) => {
   try {
-    const formData = new FormData();
+    const formData = new FormData()
     Object.keys(dataObj).forEach((key) => {
-      const value = dataObj[key];
-      formData.append(key, value);
-    });
-    const res = await baseAPI.post(url, formData);
-    return res.data;
+      const value = dataObj[key]
+      formData.append(key, value)
+    })
+    const res = await baseAPI.post(url, formData)
+    return res.data
   } catch (err: unknown) {
-    console.log(err);
+    console.log(err)
     if (err instanceof AxiosError && err?.response?.status === 403)
-      return { isLoggedIn: false };
-    return { error: err };
+      return { isLoggedIn: false }
+    return { error: err }
   }
-};
+}
