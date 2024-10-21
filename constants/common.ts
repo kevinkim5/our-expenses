90ba39763b34b82296d3d8f3c5c62e468ee10336
Kevin Kim <kevinnkimm96@gmail.com>
2024-10-21 22:44:18 +0800

    add constants, lint

--- constants/common.ts
+++ constants/common.ts
@@ -1,14 +1,18 @@
 export const AUTH_STATUS = {
-  AUTH: "authenticated",
-  LOADING: "loading",
-};
+  AUTH: 'authenticated',
+  LOADING: 'loading',
+}
+
+export const BOOLEAN_KEYS = ['Claim', 'Settle']
+
+export const DAYJS_TO_STR_Z_FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSS[Z]'
 
 export const FORM_TYPES = {
-  EXPENSE: "expense",
-  SETTLE: "settle",
-};
+  EXPENSE: 'expense',
+  SETTLE: 'settle',
+}
 
 export const PEOPLE = {
-  JAC: "Jac",
-  KEV: "Kev",
-};
+  JAC: 'Jac',
+  KEV: 'Kev',
+}
